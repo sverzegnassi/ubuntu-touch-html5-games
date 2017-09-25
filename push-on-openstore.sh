925d1712339217ef5c5b039e3574b2f5ff763466
Stefano Verzegnassi <verzegnassi.stefano@gmail.com>
2017-09-25 23:25:25 +0200

    use new cli tool

--- push-on-openstore.sh
+++ push-on-openstore.sh
@@ -14,14 +14,6 @@ echo "Click packages to upload: ${CLICK_TO_UPLOAD[*]}"
 
 for i in "${CLICK_TO_UPLOAD[@]}"
 do :
-	APP_ID=$(click info $i | jq --raw-output '.name')
-	TEST_PACKAGE_EXISTS=$(./openstore-uploader-tool/open-uapp info-auth $APP_ID)
-	if [[ "$TEST_PACKAGE_EXISTS" =~ "Cannot find app with a id:" ]]
-	then
-		echo "Pushing $i as new package"
-		./openstore-uploader-tool/open-uapp new $i
-	else
-		echo "Pushing $i as an update"
-		./openstore-uploader-tool/open-uapp update $i --force
-	fi
+	echo "Pushing $i"
+	./openstore-uploader-tool/openstore-cli upload $i
 done
